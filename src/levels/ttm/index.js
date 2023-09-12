d3fab40bf4303b0d1efa2f33e6ff3a37c6ad7b48
neeh <nicolas.gougeon.mesple@gmail.com>
2023-09-12 09:13:38 +0100

    Add remaining TTM models

--- src/levels/ttm/index.js
+++ src/levels/ttm/index.js
@@ -1,7 +1,17 @@
 import { ttm_seg7_area_1_collision } from './area1.js';
 import { ttm_seg7_area_2_collision } from './area2.js';
+import { ttm_seg7_area_3_collision } from './area3.js';
+import { ttm_seg7_area_4_collision } from './area4.js';
+import { ttm_seg7_collision_pitoune_2 } from './rolling-log.js';
+import { ttm_seg7_collision_podium_warp } from './slide-exit-podium.js';
+import { ttm_seg7_collision_ukiki_cage } from './star-cage.js';
 
 export default [
   ttm_seg7_area_1_collision,
-  ttm_seg7_area_2_collision
+  ttm_seg7_area_2_collision,
+  ttm_seg7_area_3_collision,
+  ttm_seg7_area_4_collision,
+  ttm_seg7_collision_pitoune_2,
+  ttm_seg7_collision_podium_warp,
+  ttm_seg7_collision_ukiki_cage,
 ];
