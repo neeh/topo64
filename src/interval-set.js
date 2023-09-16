64bfacf7d1969dabba70109b600b94667bff9dac
neeh <nicolas.gougeon.mesple@gmail.com>
2023-09-16 16:05:42 +0100

    Work on IntervalSets

--- src/interval-set.js
+++ src/interval-set.js
@@ -1,80 +1,140 @@
 
 export class IntervalSet {
-  constructor(min = 0, max = 1, snappingTolerance = 0) {
+  constructor(min = 0, max = 1) {
     this.min = min;
     this.max = max;
-    this.snappingTolerance = snappingTolerance;
 
     this.bounds = [];
-    this.count = 0; // unused
+    this.size = 0;
   }
 
   clear() {
     this.bounds.length = 0;
-    this.count = 0;
+    this.size = 0;
+
+    return this;
   }
 
-  getIndexBefore(t) {
-    t -= this.snappingTolerance;
+  copy(src) {
+    this.min = src.min;
+    this.max = src.max;
+
+    const b = src.bounds;
+    for (let i = 0; i < b.length; ++i) {
+      this.bounds[i] = b[i];
+    }
+    if (this.bounds.length > b.length) {
+      this.bounds.length = b.length;
+    }
+    this.size = src.size;
+
+    return this;
+  }
+
+  shift(offset, index = 0) {
+    if (offset === 0) return;
+
     const b = this.bounds;
-    let i = 1;
-    for (; i < b.length && t > b[i]; i += 2);
-    return i - 1;
+    if (offset > 0) {
+      const end = index + offset
+      for (let i = b.length - 1; i >= end; --i) {
+        b[i] = b[i - offset];
+      }
+    } else {
+      const end = b.length + offset;
+      for (let i = index; i < end; ++i) {
+        b[i] = b[i - offset];
+      }
+    }
   }
 
-  resize(newSize, pivot = this.bounds.length) {
+  // NOTE: Not sure if we want to shift() inside resize()
+  resize(newSize, shiftPivot = this.bounds.length) {
     const size = this.bounds.length;
     if (newSize === size) return;
 
-    for (let i = size; i < newSize; i += 2) {
+    for (let i = size; i < newSize; ++i) {
       this.bounds[i] = 0;
-      this.bounds[i + 1] = 0;
     }
-
-    const shift = newSize - size;
-    const step = shift < 0 ? 1 : -1;
-    const start = shift < 0 ? pivot : newSize - 1;
-    const end = shift < 0 ? newSize : pivot + shift - 1;
-
-    for (let i = start; i !== end; i += step) {
-      this.bounds[i] = this.bounds[i - shift];
-    }
-
-    if (newSize > size) {
-      // for (let i = pivot; i <= end; i += 2) {
-      //   this.bounds[i] = this.max;
-      //   this.bounds[i + 1] = this.min;
-      // }
-      for (let i = pivot; i <= end; ++i) {
-        this.bounds[i] = i & 1 ? this.min : this.max;
-      }
-    } else if (newSize < size) {
+    this.shift(newSize - size, shiftPivot);
+    if (newSize < size) {
       this.bounds.length = newSize;
     }
+    this.size = newSize;
   }
 
-  add(min = 0, max = min) {
+  add(min = 0, max = min, thickness = 0) {
     if (min < this.min) min = this.min;
     if (max > this.max) max = this.max;
     if (min >= max) return;
 
-    const minIndex = this.getIndexBefore(min);
-
     const b = this.bounds;
-    let size = b.length;
+    const size = b.length;
 
-    const t = max + this.snappingTolerance;
-    let i = minIndex;
+    let i = 1;
+    let t = min - thickness;
+    for (; i < size && t > b[i]; i += 2);
+    const minIndex = --i;
+
+    t = max + thickness;
     for (; i < size && t >= b[i]; i += 2);
+
+    const minOutside = minIndex === size || min < b[minIndex];
+    const maxOutside = i === 0 || max > b[i - 1];
 
     const indexDelta = minIndex - i + 2;
     if (indexDelta !== 0) {
-      size += indexDelta;
-      this.resize(size, minIndex);
+      this.resize(size + indexDelta, minOutside ? minIndex : minIndex + 1);
     }
 
-    if (b[minIndex] > min) b[minIndex] = min;
-    if (b[minIndex + 1] < max) b[minIndex + 1] = max;
+    if (minOutside) b[minIndex] = min;
+    if (maxOutside) b[minIndex + 1] = max;
+
+    return this;
+  }
+
+  // min   max   minIndex   i   minInside   maxInside    delta
+  // 6     9     2          4   false       true         0
+  // 9     15    2          4   true        false        0
+  // 9     15    2          4   false       false        0 -> SHOULD BE -2
+  // 8     10    2          4
+  // WIP
+  removeNew(min = 0, max = min, thickness = 0) {
+    if (min < this.min) min = this.min;
+    if (max > this.max) max = this.max;
+    if (min >= max) return;
+
+    const b = this.bounds;
+    const size = b.length;
+
+    let i = 1;
+    let t = min - thickness;
+    for (; i < size && t >= b[i]; i += 2);
+    const minIndex = --i;
+
+    t = max + thickness;
+    for (; i < size && t > b[i]; i += 2);
+
+    const minInside = minIndex > 0 && min > b[minIndex];
+    const maxInside = i > 0 && max < b[i - 1];
+
+    let indexDelta = minIndex - i + 2;
+    if (minInside && maxInside) indexDelta += 2;
+    if (!minInside && !maxInside) indexDelta -= 2;
+    if (indexDelta !== 0) {
+      this.resize(size + indexDelta, minIndex);
+    }
+
+    if (minInside && maxInside) {
+      b[minIndex + 1] = min;
+      b[minIndex + 2] = max;
+    } else if (minInside) {
+      b[minIndex + 1] = min;
+    } else if (maxInside) {
+      b[minIndex] = max;
+    }
+
+    return this;
   }
 
   remove(min = 0, max = min, thickness = 0) {
@@ -128,31 +188,50 @@ export class IntervalSet {
     } else if (minReallyInside) {
       b[minIndex] = min;
     } else if (maxReallyInside) {
-      b[minIndex] = max;
+      b[minIndex + minInside] = max;
     }
+
+    return this;
   }
 
-  getInverse(ret = []) {
+  invert() {
     const b = this.bounds;
-    let start = 0;
-    let end = b.length;
+    const size = b.length;
 
-    if (end === 0 || b[0] !== this.min) {
-      ret.push(this.min);
+    const startOnMin = size > 0 && b[0] === this.min;
+    const endOnMax = size > 0 && b[size - 1] === this.max;
+
+    if (startOnMin && endOnMax) {
+      this.shift(-1);
+      this.resize(size - 2);
+    } else if (startOnMin) {
+      this.shift(-1);
+      b[size - 1] = this.max;
+    } else if (endOnMax) {
+      this.shift(1);
+      b[0] = this.min;
     } else {
-      start = 1;
+      this.resize(size + 2);
+      this.shift(1);
+      b[0] = this.min;
+      b[size + 1] = this.max;
     }
+    return this;
+  }
 
-    const lastIsMax = end > 0 && b[end - 1] === this.max;
-    if (lastIsMax) --end;
+  union(intervals, thickness = 0) {
+    const b = intervals.bounds;
+    for (let i = 0; i < b.length; i += 2) {
+      this.add(b[i], b[i + 1], thickness);
+    }
+    return this;
+  }
 
-    // NODE: might use this.count here
-    for (let i = start; i < end; ++i) {
-      ret.push(b[i]);
+  difference(intervals, thickness = 0) {
+    const b = intervals.bounds;
+    for (let i = 0; i < b.length; i += 2) {
+      this.remove(b[i], b[i + 1], thickness);
     }
-    if (!lastIsMax) {
-      ret.push(this.max);
-    }
-    return ret;
+    return this;
   }
 }
