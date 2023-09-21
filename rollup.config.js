94219000670f56255df8c31785406ae0b5392c3b
neeh <nicolas.gougeon.mesple@gmail.com>
2023-09-21 21:19:57 +0100

    Improve build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,65 @@
+import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { createServer } from 'http-server';
 import opener from 'opener';
-import nodeResolve from '@rollup/plugin-node-resolve';
 import svelte from 'rollup-plugin-svelte';
 import css from 'rollup-plugin-css-only';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const {
   PROD = false,
+  SERVE = false,
   OPEN = false
 } = process.env;
 
-const destination = path.join(__dirname, 'public');
+const destination = path.join(__dirname, PROD ? 'dist' : '.dev');
+
+function clean() {
+  let first = true;
+  return {
+    name: 'clean',
+    buildStart() {
+      if (!first) return;
+      first = false;
+      if (fs.existsSync(destination)) {
+        fs.rmSync(destination, { recursive: true });
+      }
+    }
+  };
+}
+
+function copyDir(srcDir, destDir) {
+  fs.mkdirSync(destDir, { recursive: true });
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file);
+    if (srcFile === destDir) continue;
+
+    const destFile = path.resolve(destDir, file);
+    const stat = fs.statSync(srcFile);
+    if (stat.isDirectory()) {
+      copyDir(srcFile, destFile)
+    } else {
+      fs.copyFileSync(srcFile, destFile)
+    }
+  }
+}
+
+function copyPublicDir() {
+  let first = true;
+  return {
+    name: 'copy-public-dir',
+    writeBundle() {
+      if (!first) return;
+      first = false;
+      console.log('Copy public dir...');
+      copyDir(path.resolve(__dirname, 'public'), destination);
+    }
+  };
+}
 
 function serve() {
   const port = 8083;
@@ -51,7 +96,8 @@ export default {
     sourcemap: !PROD
   },
   plugins: [
-    svelte({ // resolveId, load, transform
+    clean(),
+    svelte({
       include: 'src/components/**/*.svelte',
       compilerOptions: {
         dev: !PROD
@@ -63,7 +109,8 @@ export default {
     nodeResolve({
       browser: true
     }),
-    serve(), // writeBundle
+    copyPublicDir(),
+    SERVE && serve(),
   ],
   watch: {
     clearScreen: false
